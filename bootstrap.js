2d5755538ab29a5683f272f55a6f7ee18bb5b298
Anthony HAMON <anthony.hamon@iadvize.com>
2015-11-18 16:16:16 +0100

    feat(vendor) : add lodash

--- bootstrap.js
+++ bootstrap.js
@@ -4,5 +4,6 @@ require('http').globalAgent.maxSockets = Infinity;
 
 global.assert = require('assert');
 global.when = require('when');
+global._ = require('lodash');
 global.Bacon = require('baconjs');
 global.not = require('not');
