f6c8dd52b665dab076a7bf377ad505ccefe15fd3
Anthony HAMON <anthony.hamon@iadvize.com>
2015-11-18 16:16:36 +0100

    feat(cli) : mv command parser to dedicated module

---
+++ cli/index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var path = require('path');
+var program = require('commander');
+var fs = require('fs');
+
+program
+  .option('-o, --owner <name>', 'Repository owner name (required).')
+  .option('-r, --repo <repo>', 'Repository name (required).')
+  .option('-t, --token <token>', 'Your GitHub token (required).')
+  .option('-s, --since <iso-date>', 'Initial date or commit sha (required).')
+  .option('-u, --until <iso-date>', 'Limit date or commit sha.')
+  .option('-p, --template <path>', 'EJS template to format data.' +
+          'The default bundled template generates a list of issues in Markdown')
+  .option('-g, --gist', 'Publish output to a Github gist.')
+  .option('-d, --data <data>', 'Set arbitrary JSON data available in the template.')
+  .option('-j, --json', 'Get output in JSON.');
+
+var checkOptions = function(options) {
+  _.forEach(options, function(value, key) {
+    if (!value) {
+      console.error('\n"%s" options must be provided', key);
+      program.help();
+      process.exit(1);
+    }
+  });
+};
+
+var parse = function(argv) {
+  var args = program.parse(argv);
+
+  checkOptions({
+    repo: args.repo,
+    owner: args.owner,
+    token: args.token,
+    since: args.since
+  });
+
+  args.template = args.template || path.join(__dirname, '../changelog.ejs');
+
+  if (!fs.existsSync(args.template)) {
+    console.error('\n"template" options is not an existing filename');
+    program.help();
+    process.exit(1);
+  }
+
+  return args;
+};
+
+module.exports = {
+  parse: parse
+};
--- index.js
+++ index.js
@@ -1,71 +1,28 @@
 #!/usr/bin/env node
 
 require('./bootstrap');
+
 var logger = require('winston');
 var config = require('./config')(logger);
-
-// core modules
 var fs = require('fs');
-var path = require('path');
+var cli = require('./cli');
 
-// 3rd party modules
-var program = require('commander');
-
-program
-  .option('-o, --owner <name>', 'Repository owner name.')
-  .option('-r, --repo <repo>', 'Repository name (required).')
-  .option('-t, --token <token>', 'Your GitHub token (only required ' +
-          'for private repos or if you want to bypass the Github API limit rate).')
-  .option('-s, --since <iso-date>', 'Initial date or commit sha (required).')
-  .option('-u, --until <iso-date>', 'Limit date or commit sha.')
-  .option('-p, --template <path>', 'EJS template to format data.' +
-          'The default bundled template generates a list of issues in Markdown')
-  .option('-g, --gist', 'Publish output to a Github gist.')
-  .option('-d, --data <data>', 'Set arbitrary JSON data available in the template.')
-  .option('-j, --json', 'Get output in JSON.')
-  .parse(process.argv);
-
-if (!program.repo) {
-  console.error('\n"repo" options must be provided');
-  program.help();
-  process.exit(1);
-}
-
-if (!program.owner) {
-  console.error('\n"owner" options must be provided');
-  program.help();
-  process.exit(1);
-}
-
-if (!program.token) {
-  console.error('\n"token" options must be provided');
-  program.help();
-  process.exit(1);
-}
-
-if (!program.since) {
-  console.error('\n"since" option must be provided');
-  program.help();
-  process.exit(1);
-}
-
-var templatePath = program.template || path.join(__dirname, 'changelog.ejs');
-var template = fs.readFileSync(templatePath, 'utf8');
-
+var args = cli.parse(process.argv);
+var template = fs.readFileSync(args.template, 'utf8');
 
 var jira = require('./jira')(config.jira);
 
 var github = require('./github')(
   {
-    token: program.token
+    token: args.token
   },
-  program.owner,
-  program.repo
+  args.owner,
+  args.repo
 );
 
 var changelog = require('./changelog')(github.api);
 
-var range = github.helper.range(program.since, program.until);
+var range = github.helper.range(args.since, args.until);
 
 var pullRequests = range
     .getPullRequests
@@ -73,13 +30,13 @@ var pullRequests = range
 
 changelog.build(
   {
-    json: program.json,
-    gist: program.gist,
-    release: program.release
+    json: args.json,
+    gist: args.gist,
+    release: args.release
   },
   template,
   range.sinceDateStream,
   range.untilDateStream,
   pullRequests,
-  program.data
+  args.data
 );
