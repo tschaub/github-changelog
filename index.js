b70570606001214efbbf3f1dfd77c38f905721fc
Anthony HAMON <anthony.hamon@iadvize.com>
2015-11-18 16:59:56 +0100

    style(index.js) : simplify

--- index.js
+++ index.js
@@ -5,29 +5,20 @@ require('./bootstrap');
 var logger = require('winston');
 var config = require('./config')(logger);
 var fs = require('fs');
-var cli = require('./cli');
 
+var cli = require('./cli');
 var args = cli.parse(process.argv);
 var template = fs.readFileSync(args.template, 'utf8');
 
 var jira = require('./jira')(config.jira);
 
-var github = require('./github')(
-  {
-    token: args.token
-  },
-  args.owner,
-  args.repo
-);
+var github = require('./github')({token: args.token}, args.owner, args.repo);
+var range = github.helper.range(args.since, args.until);
+var pullRequests = range
+      .getPullRequests
+      .flatMap(github.helper.jira(jira).fetch);
 
 var changelog = require('./changelog')(github.api);
-
-var range = github.helper.range(args.since, args.until);
-
-var pullRequests = range
-    .getPullRequests
-    .flatMap(github.helper.jira(jira).fetch);
-
 changelog.build(
   {
     json: args.json,
